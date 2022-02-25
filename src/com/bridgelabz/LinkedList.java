9055f0b01eed04a3aa02045e569d5c4d0017e006
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-25 20:54:32 +0530

    Added: Add first method to Linked List logic

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -14,4 +14,37 @@ public class LinkedList {
 			this.data = data;
 		}
 	}
+
+	public void addFirst(Object data) {
+		// 100 200 300
+		// create new node
+		Node newNode = new Node(data);// 400
+		// list is empty
+		if (head == null)
+			head = newNode;
+		// list is not empty
+		else {
+			newNode.ref = head; // head==>400 100 200 300
+			head = newNode;
+		}
+	}
+
+	public void display() {
+		// list is empty
+		if (head == null)
+			System.out.println("No elements to display.");
+		// list is not empty
+		else {
+			Node temp = head;
+			// traverse up to null elements
+			while (temp != null) {
+				if (temp.ref != null)
+					System.out.print(temp.data + " => ");
+				else
+					// display data
+					System.out.println(temp.data);
+				temp = temp.ref;
+			}
+		}
+	}
 }
--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -1,9 +1,17 @@
+/*
+ *Ability to create Linked List by adding 30 and 56 to 70
+ */
 package com.bridgelabz;
 
 public class LinkedListMain {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To Linked list problem");
+		LinkedList list = new LinkedList();
+		list.addFirst(70);
+		list.addFirst(30);
+		list.addFirst(56);
+
+		list.display();
 	}
 
 }
