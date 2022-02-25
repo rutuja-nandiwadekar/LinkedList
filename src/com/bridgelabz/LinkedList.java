c4598b10ee52258c65dc06cc3c78596a5e650601
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-25 20:17:04 +0530

    Added: simple Linked List logic

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -1,5 +1,17 @@
 package com.bridgelabz;
 
 public class LinkedList {
+	Node head;
 
+	// node creation
+	public class Node {
+
+		Object data;
+		Node ref;
+
+		// constructor
+		public Node(Object data) {
+			this.data = data;
+		}
+	}
 }
