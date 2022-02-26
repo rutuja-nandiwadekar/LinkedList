149767644292347a89591388b81e1f0d4d92bf83
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-26 19:09:50 +0530

    Added: Insert element after given data logic

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -1,5 +1,6 @@
 /*
- *Ability to search LinkedList to find Node with value 30
+ *Ability to insert 40 after 30 to the Linked List sequence of 56->30->70
+ *Final Sequence: 56->30->40->70
  */
 package com.bridgelabz;
 
@@ -8,9 +9,10 @@ public class LinkedListMain {
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
 		list.addFirst(70);
-		list.addFirst(56);
 		list.addFirst(30);
+		list.addFirst(56);
 		list.display();
-		list.search(30);
+		list.addMiddle(40);
+		list.display();
 	}
 }
